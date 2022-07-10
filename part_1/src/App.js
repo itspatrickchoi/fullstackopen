79f850845b3c7340521ed6ef88c2c14b622dffc5
Patrick Choi <mr_patrickchoi@yahoo.com>
2022-07-10 18:14:57 +0200

    unicafe step 1 setup

--- part_1/src/App.js
+++ part_1/src/App.js
@@ -1,32 +1,15 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/itspatrickchoi" target="_blank" rel="noreferrer">itspatrickchoi</a>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const name = "Peter"
-  const age = 10
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-  <>
-    <h1>Greetings</h1>
-    <Hello name="Maya" age={26 + 10} />
-    <Hello name={name} age={age} />
-    <Footer />
-  </>
+    <div>
+      code here
+    </div>
   )
 }
 
