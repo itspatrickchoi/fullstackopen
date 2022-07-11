d389d1f3b02996e426f141a50149fa9f714ee15c
Patrick Choi <mr_patrickchoi@yahoo.com>
2022-07-11 19:52:08 +0200

    1.10+1.11 unicafe step5 + step6

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -1,14 +1,49 @@
 import { useState } from 'react'
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
+const Button = (props) => {
+  return(
+    <div>
+      <button onClick={props.handleClick}>{props.text}</button>
+    </div>
+  )
+}
+
+const Buttons = (props) => {
+  return(
+    <div>
+      <Button handleClick={props.setGoodValue} text="good" />
+      <Button handleClick={props.setNeutralValue} text="neutral" />
+      <Button handleClick={props.setBadValue} text="bad" />
+    </div>
+  )
+}
+
+const StatisticLine = (props) => (
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.stat}</td>
+  </tr>
 )
 
-const Statistics = (props) => (
-  <div>
-    {props.text} {props.stat}
-  </div>
-)
+const Statistics = (props) => {
+  if (props.all === 0) {
+    return(
+      <div>No feedback given</div>
+    )
+  }
+  else {
+    return(
+      <div>
+        <StatisticLine text="good" stat={props.good} />
+        <StatisticLine text="neutral" stat={props.neutral} />
+        <StatisticLine text="bad" stat={props.bad} />
+        <StatisticLine text="all" stat={props.all} />
+        <StatisticLine text="average" stat={props.average} />
+        <StatisticLine text="positive" stat={props.positive} />
+      </div>
+    )
+  }
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -16,44 +51,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const setGoodValue = () => {
+    setGood(good+1)
+  }
   const setNeutralValue = () => {
     setNeutral(neutral+1)
   }
+  const setBadValue = () => {
+    setBad(bad+1)
+  }
 
   let all = good+neutral+bad
-  let average = (good*1 + neutral*0 -1*bad)/all
-  let positive = (good/all)*100 + " %"
+  let average = ((good*1 + neutral*0 -1*bad)/all).toFixed(2)
+  let positive = ((good/all)*100).toFixed(2) + " %"
   console.log(all, average, positive)
 
-  if (all != 0) {
-    return (
-      <>
-        <h1>get feedback</h1>
-        <Button handleClick={() => setGood(good+1)} text="good" />
-        <Button handleClick={setNeutralValue} text="neutral" />
-        <Button handleClick={() => setBad(bad+1)} text="bad" />
-        <h1>statistics</h1>
-        <Statistics text="good" stat={good} />
-        <Statistics text="neutral" stat={neutral} />
-        <Statistics text="bad" stat={bad} />
-        <Statistics text="all" stat={all} />
-        <Statistics text="average" stat={average} />
-        <Statistics text="positive" stat={positive} />
-      </>
-    )
-  }
-  else {
-    return (
-      <>
-        <h1>get feedback</h1>
-        <Button handleClick={() => setGood(good+1)} text="good" />
-        <Button handleClick={setNeutralValue} text="neutral" />
-        <Button handleClick={() => setBad(bad+1)} text="bad" />
-        <h1>statistics</h1>
-        <p>No feedback given</p>
-      </>
-    )
-  }
+  
+  return (
+    <>
+      <h1>get feedback</h1>
+      <Buttons setGoodValue={setGoodValue} setNeutralValue={setNeutralValue} setBadValue={setBadValue} />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
+    </>
+  )
 }
 
 export default App
